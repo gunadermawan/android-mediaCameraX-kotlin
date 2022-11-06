e68b0a032c9ff93ec883dff849f486039e6c9dc2
Guna Dermawan <gunadermawan@Gunas-MacBook-Pro.local>
2022-11-06 12:28:04 +0700

    create fun to upload image

--- app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
@@ -15,15 +15,24 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import com.dicoding.picodiploma.mycamera.databinding.ActivityMainBinding
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import retrofit2.Call
+import retrofit2.Response
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var currentPhotoPath: String
+    private var getFile:File?=null
     private val launchIntentCamera =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             if (it.resultCode == RESULT_OK) {
                 val myFile = File(currentPhotoPath)
+                getFile = myFile
                 val result = BitmapFactory.decodeFile(myFile.path)
                 binding.previewImageView.setImageBitmap(result)
             }
@@ -33,6 +42,7 @@ class MainActivity : AppCompatActivity() {
             if (result.resultCode == RESULT_OK) {
                 val selectedImg: Uri = result?.data?.data as Uri
                 val myFile = uriToFile(selectedImg, this@MainActivity)
+                getFile = myFile
                 binding.previewImageView.setImageURI(selectedImg)
             }
         }
@@ -101,17 +111,44 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun uploadImage() {
-        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+        if (getFile!=null){
+            val file = getFile as File
+            val description = "ini adalah deskripsi gambar".toRequestBody("text/plain".toMediaType())
+            val requestImage = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
+            val imageMultipart:MultipartBody.Part = MultipartBody.Part.createFormData("photo",file.name,requestImage)
+            val service=ApiConfig().getApiService().uploadImage(imageMultipart,description)
+            service.enqueue(object :retrofit2.Callback<FileUploadResponse>{
+                override fun onResponse(
+                    call: Call<FileUploadResponse>,
+                    response: Response<FileUploadResponse>
+                ) {
+                    if (response.isSuccessful){
+                        val responseBody = response.body()
+                        if (responseBody!=null && !responseBody.error){
+                            Toast.makeText(this@MainActivity, response.message(), Toast.LENGTH_SHORT).show()
+                        }
+                    }else{
+                        Toast.makeText(this@MainActivity, response.message(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<FileUploadResponse>, t: Throwable) {
+                    Toast.makeText(this@MainActivity, "failed to upload image!", Toast.LENGTH_SHORT).show()
+                }
+
+            })
+        }else{
+            Toast.makeText(this, "silahkan upload file terlebih dahulu", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private val launcherIntentCameraX =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             if (it.resultCode == CAMERA_X_RESULT) {
                 val myFile = it.data?.getSerializableExtra("picture") as File
+                getFile = myFile
                 val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
-
                 val result = rotateBitmap(BitmapFactory.decodeFile(myFile.path), isBackCamera)
-
                 binding.previewImageView.setImageBitmap(result)
             }
         }
